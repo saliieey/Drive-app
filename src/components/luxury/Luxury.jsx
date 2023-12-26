7fc02bd0c0dc2e45cb6db99c6c228857e3163d04
saliieey <salihck7777@gmail.com>
2023-12-26 12:39:00 +0530

    updated

--- src/components/luxury/Luxury.jsx
+++ src/components/luxury/Luxury.jsx
@@ -16,25 +16,25 @@ const Luxury = () => {
 
       <div className={styles.container}>
         <div className={styles.card}>
-            <img src="https://images.pexels.com/photos/3764984/pexels-photo-3764984.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <a href='https://www.rolls-roycemotorcars.com/content/rrmc/marketUK/rollsroycemotorcars_com/en_GB/showroom.html'><img src="https://images.pexels.com/photos/3764984/pexels-photo-3764984.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /></a>
             <div className={styles.content}>
                 <h3>Rolls Royce</h3>
             </div>
         </div>
         <div className={styles.card}>
-            <img src="https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <a href='https://www.maserati.com/in/en/models'><img src="https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /></a>
             <div className={styles.content}>
                 <h3>Maserati</h3>
             </div>
         </div>
         <div className={styles.card}>
-            <img src="https://images.unsplash.com/photo-1563720360172-67b8f3dce741?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJhbmdlJTIwcm92ZXJ8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+           <a href='https://www.landrover.in/range-rover/range-rover/index.html'> <img src="https://images.unsplash.com/photo-1563720360172-67b8f3dce741?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJhbmdlJTIwcm92ZXJ8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" /></a>
             <div className={styles.content}>
                 <h3>Range Rover</h3>
             </div>
         </div>
         <div className={styles.card}>
-            <img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGx1eHVyeSUyMGNhcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+            <a href='https://www.landrover.in/range-rover/range-rover/index.html'><img src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGx1eHVyeSUyMGNhcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" /></a>
             <div className={styles.content}>
                 <h3>Porsche</h3>
             </div>
